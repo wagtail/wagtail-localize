5e84201d4b19a7a46710da808fb88b23c210e834
Karl Hobley <karl@torchbox.com>
2019-03-27 13:14:33 +0000

    Fix typo

--- wagtail_i18n/plugins/segmentizer/apps.py
+++ wagtail_i18n/plugins/segmentizer/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class WagtailI18NSegmentizerAppConfig(AppConfig):
     label = 'wagtail_i18n_segmentizer'
     name = 'wagtail_i18n.plugins.segmentizer'
-    verbose_name = "Wagtail I18N Regmentizer"
+    verbose_name = "Wagtail I18N Segmentizer"
 
