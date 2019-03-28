6f654124663206a77ae56ab124da27ceb884be72
Karl Hobley <karl@torchbox.com>
2019-03-28 11:04:15 +0000

    Improvements to Google Translate engine

--- wagtail_i18n/plugins/translation_engines/google_translate/views.py
+++ wagtail_i18n/plugins/translation_engines/google_translate/views.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.contrib import messages
 from django.db import transaction
 from django.views.decorators.http import require_POST
@@ -5,7 +7,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.utils.text import slugify
 
 from wagtail_i18n.plugins.workflow.models import TranslationRequest
-from wagtail_i18n.segments import SegmentValue
+from wagtail_i18n.segments import SegmentValue, TemplateValue
 from wagtail_i18n.segments.extract import extract_segments
 from wagtail_i18n.segments.ingest import ingest_segments
 
@@ -23,23 +25,26 @@ def translate(request, translation_request_id):
 
         segments = extract_segments(instance)
 
-        # TODO: Process non-plaintext segments
+        text_segments = [segment for segment in segments if isinstance(segment, SegmentValue)]
+        template_segments = [segment for segment in segments if isinstance(segment, TemplateValue)]
+
+        # Group segments by source text so we only submit them once
+        text_segments_grouped = defaultdict(list)
+        for segment in text_segments:
+            text_segments_grouped[segment.text].append(segment.path)
+
         translations = translator.translate(
-            [segment.text for segment in segments],
-            src='en',
+            list(text_segments_grouped.keys()),
+            src='en',  # FIXME (Google Translate doesn't like 'en-gb')
             dest=translation_request.target_locale.language.code,
         )
 
-        # FIXME: This will break if multiple segments have the same text
-        source_to_path_mapping = {
-            segment.text: segment.path
-            for segment in segments
-        }
-
-        translated_segments = [
-            SegmentValue(source_to_path_mapping[t.origin], t.text)
-            for t in translations
-        ]
+        translated_segments = template_segments.copy()
+        for translation in translations:
+            translated_segments.extend([
+                SegmentValue(path, translation.text)
+                for path in text_segments_grouped[translation.origin]
+            ])
 
         with transaction.atomic():
             try:
