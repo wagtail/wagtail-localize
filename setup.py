882f0b6f6f1792dbe008a2609562156349c354b3
Karl Hobley <karl@kaed.uk>
2019-05-21 17:17:21 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys, os
+
+from setuptools import setup, find_packages
+
+# Hack to prevent "TypeError: 'NoneType' object is not callable" error
+# in multiprocessing/util.py _exit_function when setup.py exits
+# (see http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+setup(
+    name='wagtail-i18n',
+    version='0.1',
+    description='Wagtail Internationalisation Framework',
+    author='Karl Hobley',
+    author_email='karl@kaed.uk',
+    url='',
+    packages=find_packages(),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[],
+    zip_safe=False,
+)
