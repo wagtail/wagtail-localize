64df1d0f199650097712bd89fa0b8785c141a5a7
Karl Hobley <karl@kaed.uk>
2019-07-23 17:51:21 +0100

    Moved regions_admin module to admin folder

--- wagtail_localize/admin/regions/apps.py
+++ wagtail_localize/admin/regions/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class WagtailLocalizeRegionsAdminAppConfig(AppConfig):
     label = 'wagtail_localize_regions_admin'
-    name = 'wagtail_localize.plugins.regions_admin'
+    name = 'wagtail_localize.admin.regions'
     verbose_name = "Wagtail Localize Regions admin"
