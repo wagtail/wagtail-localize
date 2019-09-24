e9ae050b737b5e8167ead80d92961666dd474f5c
Karl Hobley <karl@kaed.uk>
2019-09-24 12:42:11 +0100

    Set include_package_data to True in setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     author_email='karl@kaed.uk',
     url='',
     packages=find_packages(),
+    include_package_data=True,
     license='BSD',
     classifiers=[
         'Development Status :: 3 - Alpha',
