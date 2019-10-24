88bb159a4525860db642444482be0cc58bdb6546
Karl Hobley <karl@kaed.uk>
2019-10-24 16:50:10 +0100

    Added tests for Language model

---
+++ wagtail_localize/tests/test_language_region_locale_models.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+from django.test import TestCase, override_settings
+from django.utils import translation
+
+from wagtail_localize.models import Language
+
+
+class TestLanguageModel(TestCase):
+    def setUp(self):
+        language_codes = dict(settings.LANGUAGES).keys()
+
+        for language_code in language_codes:
+            Language.objects.update_or_create(
+                code=language_code, defaults={"is_active": True}
+            )
+
+    def test_default(self):
+        default_language = Language.objects.default()
+        self.assertEqual(default_language.code, "en")
+        self.assertEqual(Language.objects.default_id(), default_language.id)
+
+    @override_settings(LANGUAGE_CODE="fr-ca")
+    def test_default_doesnt_have_to_be_english(self):
+        default_language = Language.objects.default()
+        self.assertEqual(default_language.code, "fr")
+        self.assertEqual(Language.objects.default_id(), default_language.id)
+
+    def test_get_active_default(self):
+        self.assertEqual(Language.get_active().code, "en")
+
+    def test_get_active_overridden(self):
+        with translation.override("fr"):
+            self.assertEqual(Language.get_active().code, "fr")
+
+    def test_as_rfc5646_language_tag(self):
+        tests = [("en", "en"), ("en-gb", "en-GB"), ("zh-hans", "zh-Hans")]
+
+        for code, expected in tests:
+            with self.subTest(code=code):
+                result = Language(code=code).as_rfc5646_language_tag()
+                self.assertEqual(result, expected)
+
+    def test_get_by_rfc5646_language_tag(self):
+        language = Language.objects.create(code="en-gb")
+
+        got_language = Language.get_by_rfc5646_language_tag("en-GB")
+
+        self.assertEqual(language, got_language)
+
+    def test_get_display_name(self):
+        language = Language.objects.get(code="en")
+        self.assertEqual(language.get_display_name(), "English")
+
+    def test_str(self):
+        language = Language.objects.get(code="en")
+        self.assertEqual(str(language), "English (en)")
