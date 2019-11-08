c31cbd7f5bea4bb537b1b7b51795fad75f5cbfdf
Karl Hobley <karl@kaed.uk>
2019-11-08 11:20:22 +0000

    Don't escape segments in restore_html_segments

--- wagtail_localize/segments/html.py
+++ wagtail_localize/segments/html.py
@@ -167,12 +167,12 @@ def extract_html_segments(html):
     return str(soup), segments
 
 
-def restore_html_segments(template, texts):
+def restore_html_segments(template, segments):
     soup = BeautifulSoup(template, "html.parser")
 
     for text_element in soup.findAll("text"):
-        value = texts[int(text_element.get("position"))]
-        text_element.replaceWith(value.strip())
+        value = segments[int(text_element.get("position"))]
+        text_element.replaceWith(BeautifulSoup(value.strip(), "html.parser"))
 
     return str(soup)
 
