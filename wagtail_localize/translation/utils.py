efd6493b996f41ff7357013aa28788cfce962c57
Karl Hobley <karl@torchbox.com>
2020-04-06 19:49:20 +0100

    Check related objects in get_translation_progress

--- wagtail_localize/translation/utils.py
+++ wagtail_localize/translation/utils.py
@@ -14,6 +14,7 @@ from wagtail_localize.translation.segments import TemplateValue, RelatedObjectVa
 from wagtail_localize.translation.segments.extract import extract_segments
 
 from .models import (
+    pk,
     Segment,
     SegmentTranslation,
     SegmentLocation,
@@ -23,17 +24,26 @@ from .models import (
 )
 
 
-def get_translation_progress(revision_id, locale):
+class TranslationProgress:
+    def __init__(self, total_segments, translated_segments, total_related_objects, translated_related_objects):
+        self.total_segments = total_segments
+        self.translated_segments = translated_segments
+        self.total_related_objects = total_related_objects
+        self.translated_related_objects = translated_related_objects
+
+    def is_ready(self):
+        return self.total_segments == self.translated_segments and self.total_related_objects == self.translated_related_objects
+
+
+def get_translation_progress(revision, locale):
     """
     For the specified page revision, get the current
     translation progress into the specified locale.
 
-    Returns two integers:
-     - The total number of segments in the revision to translate
-     - The number of segments that have been translated into the locale
+    The revision/locale arguments can either be objects or IDs
     """
     # Get QuerySet of Segments that need to be translated
-    required_segments = SegmentLocation.objects.filter(revision_id=revision_id)
+    required_segments = SegmentLocation.objects.filter(revision_id=pk(revision))
 
     # Annotate each Segment with a flag that indicates whether the segment is translated
     # into the specified locale
@@ -42,13 +52,13 @@ def get_translation_progress(revision_id, locale):
             SegmentTranslation.objects.filter(
                 translation_of_id=OuterRef("segment_id"),
                 context_id=OuterRef("context_id"),
-                locale=locale,
+                locale_id=pk(locale),
             )
         )
     )
 
     # Count the total number of segments and the number of translated segments
-    aggs = required_segments.annotate(
+    segment_aggs = required_segments.annotate(
         is_translated_i=Case(
             When(is_translated=True, then=Value(1)),
             default=Value(0),
@@ -56,7 +66,22 @@ def get_translation_progress(revision_id, locale):
         )
     ).aggregate(total_segments=Count("pk"), translated_segments=Sum("is_translated_i"))
 
-    return aggs["total_segments"], aggs["translated_segments"]
+    # Work out how many required related objects have been translated
+    required_related_objects = RelatedObjectLocation.objects.filter(revision_id=pk(revision))
+    total_related_objects = 0
+    translated_related_objects = 0
+    for related_object in required_related_objects:
+        total_related_objects += 1
+
+        if related_object.has_translation(locale):
+            translated_related_objects += 1
+
+    return TranslationProgress(
+        segment_aggs["total_segments"],
+        segment_aggs["translated_segments"],
+        total_related_objects,
+        translated_related_objects,
+    )
 
 
 def insert_segments(revision, locale, segments):
