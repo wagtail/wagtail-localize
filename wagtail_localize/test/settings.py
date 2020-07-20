ae193cb3f64eeb5bc4b3fcb7c94fd1d75771e9ed
Karl Hobley <karl@torchbox.com>
2020-07-20 10:46:01 +0100

    Remove SiteMiddleware

--- wagtail_localize/test/settings.py
+++ wagtail_localize/test/settings.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.locale.LocaleMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
