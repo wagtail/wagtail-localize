f581b7a1e8c8395a2c48944d9828946b6c5443ec
Karl Hobley <karl@torchbox.com>
2020-07-20 17:31:29 +0100

    Implemented a dummy translator

---
+++ wagtail_localize/machine_translators/dummy.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup, NavigableString, Tag
+
+from wagtail_localize.strings import lstrip_keep, rstrip_keep, StringValue
+
+from .base import BaseMachineTranslator
+
+
+def language_code(code):
+    return code.split("-")[0]
+
+
+def translate_string(string):
+    # Preserve leading/trailing whitespace. This could be a segment of a sentence.
+    string, left_whitespace = lstrip_keep(string)
+    string, right_whitespace = rstrip_keep(string)
+
+    words = string.split(' ')
+
+    return left_whitespace + ' '.join(reversed(words)) + right_whitespace
+
+
+def translate_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    def walk(soup):
+        for child in soup.children:
+            if isinstance(child, NavigableString):
+                # Translate navigable strings
+                child.string.replace_with(translate_string(child.string))
+
+            else:
+                walk(child)
+
+    # Reverse the children
+    if isinstance(soup, Tag):
+        soup.contents.reverse()
+
+    walk(soup)
+
+    return str(soup)
+
+
+class DummyTranslator(BaseMachineTranslator):
+    def translate(self, source_locale, target_locale, strings):
+        return {
+            string: StringValue(translate_html(string.data)) for string in strings
+        }
+
+    def can_translate(self, source_locale, target_locale):
+        return language_code(source_locale.language_code) != language_code(target_locale.language_code)
