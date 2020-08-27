16f079f5d4a311c32168fee781d831297d2fd571
Karl Hobley <karl@torchbox.com>
2020-08-27 11:44:03 +0100

    Synchronise child relations before translation

--- wagtail_localize/fields.py
+++ wagtail_localize/fields.py
@@ -1,4 +1,7 @@
+from django.db import models
+from modelcluster.fields import ParentalKey
 from wagtail.core.fields import StreamField
+from wagtail.core.models import TranslatableMixin
 
 
 class BaseTranslatableField:
@@ -41,8 +44,14 @@ class TranslatableField(BaseTranslatableField):
         return True
 
     def is_synchronized(self, obj):
+        field = self.get_field(obj.__class__)
+
+        # Child relations should all be synchronised before translation
+        if isinstance(field, (models.ManyToOneRel)) and isinstance(field.remote_field, ParentalKey):
+            return True
+
         # Streamfields need to be re-synchronised before translation so the structure and non-translatable content is copied over
-        return isinstance(self.get_field(obj.__class__), StreamField)
+        return isinstance(field, StreamField)
 
 
 class SynchronizedField(BaseTranslatableField):
@@ -52,3 +61,47 @@ class SynchronizedField(BaseTranslatableField):
 
     def is_synchronized(self, obj):
         return self.is_editable(obj)
+
+
+def copy_synchronised_fields(source, target):
+    """
+    Copies data in synchronised fields from the source object to the target object.
+    """
+    for translatable_field in getattr(source, 'translatable_fields', []):
+        if translatable_field.is_synchronized(source):
+            field = translatable_field.get_field(target.__class__)
+
+            if isinstance(field, (models.ManyToOneRel)) and isinstance(field.remote_field, ParentalKey):
+                # Use modelcluster's copy_child_relation for child relations
+
+                if issubclass(field.related_model, TranslatableMixin):
+                    # Get a list of the primary keys for the existing child objects
+                    existing_pks_by_translation_key = {
+                        child_object.translation_key: child_object.pk
+                        for child_object in getattr(target, field.name).all()
+                    }
+
+                    # Copy the new child objects across (note this replaces existing ones)
+                    child_object_map = source.copy_child_relation(field.name, target)
+
+                    # Update locale of each child object and recycle PK
+                    for (child_relation, source_pk), child_objects in child_object_map.items():
+                        if source_pk is None:
+                            # This is a list of the child objects that were never saved
+                            for child_object in child_objects:
+                                child_object.pk = existing_pks_by_translation_key.get(child_object.translation_key)
+                                child_object.locale = target.locale
+                        else:
+                            child_object = child_objects
+
+                            child_object.pk = existing_pks_by_translation_key.get(child_object.translation_key)
+                            child_object.locale = target.locale
+
+                else:
+                    source.copy_child_relation(field.name, target)
+
+            else:
+                # For all other fields, just set the attribute
+                setattr(
+                    target, field.attname, getattr(source, field.attname)
+                )
