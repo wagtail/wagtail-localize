5aac146bdab22192f4161292face2ca06f44fadc
Karl Hobley <karl@torchbox.com>
2020-09-03 20:17:23 +0100

    Moved styled tags outside of components

--- wagtail_localize/static_src/src/common/components/Tabs/index.tsx
+++ wagtail_localize/static_src/src/common/components/Tabs/index.tsx
@@ -13,14 +13,14 @@ export interface TabsProps {
     tabs: Tab[];
 }
 
+// Remove bottom margin that Wagtail adds by default
+// This makes it tricky to align the toolbox consistently when there are both tabs and no tabs
+const ULWithoutMargin = styled.ul`
+    margin-bottom: 0;
+`;
+
 export const Tabs: FunctionComponent<TabsProps> = ({tabs, children}) => {
     const [currentTab, setCurrentTab] = React.useState(tabs[0].slug)
-
-    // Remove bottom margin that Wagtail adds by default
-    // This makes it tricky to align the toolbox consistently when there are both tabs and no tabs
-    const ULWithoutMargin = styled.ul`
-        margin-bottom: 0;
-    `;
 
     return <>
         <ULWithoutMargin className="tab-nav merged" role="tablist">
@@ -53,14 +53,14 @@ export const Tabs: FunctionComponent<TabsProps> = ({tabs, children}) => {
     </>;
 }
 
+// Remove top padding that Wagtail adds by default
+// This makes it tricky to align the toolbox consistently when there are both tabs and no tabs
+const SectionWithoutPadding = styled.section`
+    padding-top: 0 !important;
+`;
+
 export const TabContent: FunctionComponent<Tab> = ({slug, children}) => {
     const currentTab = React.useContext(CurrentTabContext);
-
-    // Remove top padding that Wagtail adds by default
-    // This makes it tricky to align the toolbox consistently when there are both tabs and no tabs
-    const SectionWithoutPadding = styled.section`
-        padding-top: 0 !important;
-    `;
 
     return <SectionWithoutPadding id={`tab-${slug}`} className={slug == currentTab ? 'active' : ''}>
         {children}
