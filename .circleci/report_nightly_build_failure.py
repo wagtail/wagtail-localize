4988580a6b4b3253c7fa28551c8fcd172db21294
Karl Hobley <karl@torchbox.com>
2020-09-29 16:40:29 +0100

    Add script to report nightly build errors to Wagtail Slack

---
+++ .circleci/report_nightly_build_failure.py
@@ -0,0 +1,19 @@
+"""
+Called by CircleCI when the nightly build fails.
+
+This reports an error to the #nightly-build-failures Slack channel.
+"""
+import os
+import requests
+
+if 'SLACK_WEBHOOK_URL' in os.environ:
+    print("Reporting to #nightly-build-failures slack channel")
+    response = requests.post(os.environ['SLACK_WEBHOOK_URL'], json={
+        "text": "A Nightly build failed. See " + os.environ['CIRCLE_BUILD_URL'],
+    })
+
+    print("Slack responded with:", response)
+    print(response.content)
+
+else:
+    print("Unable to report to #nightly-build-failures slack channel because SLACK_WEBHOOK_URL is not set")
