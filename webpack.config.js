bbaaeefdcf1cee2694527f9e668e5e45d4f3b991
Karl Hobley <karl@kaed.uk>
2020-10-08 11:28:31 +0100

    Remove SASS tooling
    We don't need it and it seems to have a dependency on python2

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,6 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
         test: /\.css$/,
         use: [
           'style-loader',
