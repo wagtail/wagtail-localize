3226f0ccbdad45e9ccbb8c7e354515125ee37638
Karl Hobley <karl@kaed.uk>
2020-11-23 15:30:42 +0000

    Revert "Don't localize names"
    This reverts commit 67e90fd0eade93f861c588aedf5afbf93f61fd9f.

--- wagtail_localize/machine_translators/deepl.py
+++ wagtail_localize/machine_translators/deepl.py
@@ -1,4 +1,5 @@
 import requests
+from django.utils.translation import gettext_lazy as _
 
 from wagtail_localize.strings import StringValue
 
@@ -14,15 +15,15 @@ def language_code(code, is_target=False):
 
 
 class DeepLTranslator(BaseMachineTranslator):
-    display_name = "DeepL"
+    display_name = _("DeepL")
 
-    def translate(self, source_language_code, target_language_code, strings):
+    def translate(self, source_locale, target_locale, strings):
         response = requests.post('https://api.deepl.com/v2/translate', {
             'auth_key': self.options['AUTH_KEY'],
             'text': [string.data for string in strings],
             'tag_handling': 'xml',
-            'source_lang': language_code(source_language_code),
-            'target_lang': language_code(target_language_code, is_target=True),
+            'source_lang': language_code(source_locale.language_code),
+            'target_lang': language_code(target_locale.language_code, is_target=True),
         })
 
         return {
@@ -30,5 +31,5 @@ class DeepLTranslator(BaseMachineTranslator):
             for string, translation in zip(strings, response.json()['translations'])
         }
 
-    def can_translate(self, source_language_code, target_language_code):
-        return language_code(source_language_code) != language_code(target_language_code, is_target=True)
+    def can_translate(self, source_locale, target_locale):
+        return language_code(source_locale.language_code) != language_code(target_locale.language_code, is_target=True)
