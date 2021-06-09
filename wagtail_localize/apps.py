116582ccd39dae39d4c5dc0a34acbdb3b6984f5a
Sih Sîng-hông薛丞宏 <ihcaoe@gmail.com>
2021-06-09 16:48:09 +0800

    DEFAULT_AUTO_FIELD for django 3.2
    For #407

--- wagtail_localize/apps.py
+++ wagtail_localize/apps.py
@@ -5,6 +5,7 @@ class WagtailLocalizeAppConfig(AppConfig):
     name = 'wagtail_localize'
     label = 'wagtail_localize'
     verbose_name = "Wagtail localize"
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         from .models import register_post_delete_signal_handlers
