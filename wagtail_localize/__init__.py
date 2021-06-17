a2a1c0adc03f09a7e3595ede43905165e1eb6815
Karl Hobley <karl@torchbox.com>
2021-06-17 16:57:05 +0100

    Bump version to 1.0rc2

--- wagtail_localize/__init__.py
+++ wagtail_localize/__init__.py
@@ -5,6 +5,6 @@ default_app_config = 'wagtail_localize.apps.WagtailLocalizeAppConfig'
 
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, 'rc', 1)
+VERSION = (1, 0, 0, 'rc', 2)
 
 __version__ = get_version(VERSION)
