6b2cce35771a97fbffcc0ed066e0e17621fea88e
Dan Braghis <dan.braghis@torchbox.com>
2021-11-09 17:21:35 +0000

    Use BaseComponentManager for the locales views ComponentManager

--- wagtail_localize/locales/views.py
+++ wagtail_localize/locales/views.py
@@ -1,6 +1,5 @@
 import functools
 
-from django import forms
 from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.utils.translation import gettext_lazy
@@ -13,6 +12,8 @@ from wagtail.admin.views import generic
 from wagtail.admin.viewsets.model import ModelViewSet
 from wagtail.core.models import Locale
 from wagtail.core.permissions import locale_permission_policy
+
+from wagtail_localize.components import BaseComponentManager
 
 from .components import LocaleComponentModelForm, get_locale_components
 from .forms import LocaleForm
@@ -32,54 +33,16 @@ def get_locale_component_edit_handler(model):
         )
 
 
-class ComponentManager:
-    def __init__(self, components):
-        self.components = components
+class ComponentManager(BaseComponentManager):
+    @classmethod
+    def get_components(cls):
+        return get_locale_components()
 
     @classmethod
-    def from_request(cls, request, instance=None):
-        components = []
+    def get_component_edit_handler(cls, component_model):
+        return get_locale_component_edit_handler(component_model)
 
-        for component in get_locale_components():
-            component_model = component["model"]
-
-            component_instance = component_model.objects.filter(locale=instance).first()
-            edit_handler = get_locale_component_edit_handler(component_model).bind_to(
-                model=component_model, instance=component_instance, request=request
-            )
-            form_class = edit_handler.get_form_class()
-
-            # Add an 'enabled' field to the form if it isn't required
-            if not component["required"]:
-                form_class = type(
-                    form_class.__name__,
-                    (form_class,),
-                    {
-                        "enabled": forms.BooleanField(
-                            initial=component_instance is not None
-                        ),
-                        # Move enabled field to top
-                        "field_order": ["enabled"],
-                    },
-                )
-
-            prefix = "component-{}".format(component_model._meta.db_table)
-
-            if request.method == "POST":
-                form = form_class(
-                    request.POST,
-                    request.FILES,
-                    instance=component_instance,
-                    prefix=prefix,
-                )
-            else:
-                form = form_class(instance=component_instance, prefix=prefix)
-
-            components.append((component, component_instance, form))
-
-        return cls(components)
-
-    def is_valid(self, locale):
+    def is_valid(self, locale, *args, **kwargs):
         is_valid = True
 
         for component, _component_instance, component_form in self.components:
@@ -96,7 +59,7 @@ class ComponentManager:
 
         return is_valid
 
-    def save(self, locale):
+    def save(self, locale, *args, **kwargs):
         for component, component_instance, component_form in self.components:
             if component["required"] or component_form["enabled"].value():
                 component_instance = component_form.save(commit=False)
@@ -105,9 +68,6 @@ class ComponentManager:
 
             elif component_instance:
                 component_instance.delete()
-
-    def __iter__(self):
-        return iter(self.components)
 
 
 class IndexView(generic.IndexView):
