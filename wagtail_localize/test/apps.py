31f9dd13c9637e824109462281b0ad819d568904
Dan Braghis <dan.braghis@torchbox.com>
2021-11-24 12:37:09 +0000

    Add missing default_auto_field in test app

--- wagtail_localize/test/apps.py
+++ wagtail_localize/test/apps.py
@@ -5,3 +5,4 @@ class WagtailLocalizeTestAppConfig(AppConfig):
     label = "wagtail_localize_test"
     name = "wagtail_localize.test"
     verbose_name = "Wagtail Localize Test app"
+    default_auto_field = "django.db.models.AutoField"
