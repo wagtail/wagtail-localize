23d0bb7a529634fda0cb78a9c94120ef58b9b1a0
Dan Braghis <dan.braghis@torchbox.com>
2022-05-20 15:36:55 +0100

    Bump version to 1.2

--- wagtail_localize/version.py
+++ wagtail_localize/version.py
@@ -42,6 +42,6 @@ def get_complete_version(version=None):
 
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 1, 2, "final", 1)
+VERSION = (1, 2, 0, "final", 1)
 
 __version__ = get_version(VERSION)
