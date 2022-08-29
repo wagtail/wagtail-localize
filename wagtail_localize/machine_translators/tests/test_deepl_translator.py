d3cad72d2220088010969b4692eddf00fb2cd3e3
Rami Boutassghount <33960822+ramiboutas@users.noreply.github.com>
2022-08-29 14:38:31 +0200

    simple tests added -> auth_key_is_free_account

--- wagtail_localize/machine_translators/tests/test_deepl_translator.py
+++ wagtail_localize/machine_translators/tests/test_deepl_translator.py
@@ -1,1 +1,16 @@
 
+from django.test import TestCase
+
+from wagtail_localize.machine_translators.deepl import auth_key_is_free_account
+
+
+class TestDeeplTranslator(TestCase):
+    def setUp(self):
+        self.free_auth_key = "asd-23-ssd-243-adsf-dummy-auth-key:fx"
+        self.paid_auth_key = "asd-23-ssd-243-adsf-dummy-auth-key:bla"
+    
+    def test_api_free(self):
+        self.assertEqual(auth_key_is_free_account(self.free_auth_key), True)
+    
+    def test_api_paid(self):
+        self.assertEqual(auth_key_is_free_account(self.paid_auth_key), False)
