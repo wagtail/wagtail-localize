9bc761a78b9818bb791f7c286603b57ca1340d19
Rami Boutassghount <33960822+ramiboutas@users.noreply.github.com>
2022-08-30 11:17:09 +0200

    Update test_deepl_translator.py

--- wagtail_localize/machine_translators/tests/test_deepl_translator.py
+++ wagtail_localize/machine_translators/tests/test_deepl_translator.py
@@ -1,16 +1,19 @@
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
-from wagtail_localize.machine_translators.deepl import auth_key_is_free_account
+from wagtail_localize.machine_translators.deepl import DeepLTranslator
 
 
 class TestDeeplTranslator(TestCase):
     def setUp(self):
-        self.free_auth_key = "asd-23-ssd-243-adsf-dummy-auth-key:fx"
-        self.paid_auth_key = "asd-23-ssd-243-adsf-dummy-auth-key:bla"
+        pass
     
-    def test_api_free(self):
-        self.assertEqual(auth_key_is_free_account(self.free_auth_key), True)
+    @override_settings(WAGTAILLOCALIZE_MACHINE_TRANSLATOR["OPTIONS"]["AUTH_KEY"]="asd-23-ssd-243-adsf-dummy-auth-key:fx")
+    def test_free_api_endpoint(self):
+        free_api_endpoint = DeepLTranslator({}).get_api_endpoint()
+        self.assertEqual(free_api_endpoint, "https://api-free.deepl.com/v2/translate")
     
-    def test_api_paid(self):
-        self.assertEqual(auth_key_is_free_account(self.paid_auth_key), False)
+    @override_settings(WAGTAILLOCALIZE_MACHINE_TRANSLATOR["OPTIONS"]["AUTH_KEY"]="asd-23-ssd-243-adsf-dummy-auth-key:bla")
+    def test_paid_api_endpoint(self):
+        paid_api_endpoint = DeepLTranslator({}).get_api_endpoint()
+        self.assertEqual(paid_api_endpoint, "https://api.deepl.com/v2/translate")
