f7de2f3eb22d95f96e886828740c596bcad8c50e
Dan Braghis <dan.braghis@torchbox.com>
2022-09-19 23:39:30 +0100

    Drop pre-Django 3.2 conditionals

--- wagtail_localize/__init__.py
+++ wagtail_localize/__init__.py
@@ -1,10 +1,1 @@
-from django import VERSION as DJANGO_VERSION
-
 from .version import VERSION, __version__  # noqa
-
-
-if DJANGO_VERSION >= (3, 2):
-    # The declaration is only needed for older Django versions
-    pass
-else:
-    default_app_config = "wagtail_localize.apps.WagtailLocalizeAppConfig"
