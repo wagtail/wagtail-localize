95b2bb4adbaca575eeb00df0050fa7db9c89cc2a
Andy Babic <andy.babic@torchbox.com>
2023-01-22 13:09:58 +0000

    Improve authentication options for `GoogleCloudTranslator` (#645)
    * Improve authentication options for GoogleCloudTranslator
    * Update docs
    * Add tests without mocks (expected to fail)
    * Add mocking to tests

--- wagtail_localize/machine_translators/google.py
+++ wagtail_localize/machine_translators/google.py
@@ -1,4 +1,6 @@
+from django.utils.functional import cached_property
 from google.cloud import translate
+from google.oauth2 import service_account
 
 from wagtail_localize.strings import StringValue
 
@@ -8,12 +10,30 @@ from .base import BaseMachineTranslator
 class GoogleCloudTranslator(BaseMachineTranslator):
     display_name = "Google Translate"
 
+    @cached_property
+    def client(self):
+        # use CREDENTIALS dict, if supplied
+        credentials = self.options.get("CREDENTIALS")
+        if credentials:
+            return translate.TranslationServiceClient(
+                credentials=service_account.Credentials.from_service_account_info(
+                    credentials
+                )
+            )
+        # load key from 'CREDENTIALS_PATH', if supplied
+        path = self.options.get("CREDENTIALS_PATH")
+        if path:
+            return translate.TranslationServiceClient(
+                credentials=service_account.Credentials.from_service_account_file(path)
+            )
+        # load key from env["GOOGLE_APPLICATION_CREDENTIALS"]
+        return translate.TranslationServiceClient()
+
     def translate(self, source_locale, target_locale, strings):
-        client = translate.TranslationServiceClient()
         project_id = self.options["PROJECT_ID"]
         location = self.options.get("LOCATION", "global")
 
-        response = client.translate_text(
+        response = self.client.translate_text(
             request={
                 "parent": f"projects/{project_id}/locations/{location}",
                 "contents": [string.data for string in strings],
