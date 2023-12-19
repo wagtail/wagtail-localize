b8398440e9f49a44421c2063b3dc6f9388baa6dd
Dominick Rivard <643002+drivard@users.noreply.github.com>
2023-12-19 14:13:42 -0500

    Fix unit tests for translating text and HTML using LibreTranslate to mock the POST request

--- wagtail_localize/machine_translators/tests/test_libretranslate_translator.py
+++ wagtail_localize/machine_translators/tests/test_libretranslate_translator.py
@@ -1,3 +1,5 @@
+import json
+
 from unittest import mock
 
 from django.test import TestCase, override_settings
@@ -40,59 +42,107 @@ class TestLibreTranslator(TestCase):
         )
         self.assertEqual(self.translator.language_code("foo-bar-baz"), "foo")
 
-    @mock.patch(
-        "wagtail_localize.machine_translators.libretranslate.LibreTranslator.translate",
-        return_value={
+    @mock.patch("requests.post")
+    def test_translate_text(self, mock_post):
+        # Mock the response of requests.post
+        mock_response = mock.Mock()
+        mock_response.json.return_value = {
+            "translatedText": [
+                "Bonjour le monde!",
+                "Ceci est une phrase. Ceci est une autre phrase.",
+            ]
+        }
+        mock_response.raise_for_status = mock.Mock()
+        mock_post.return_value = mock_response
+
+        input_strings = [
+            StringValue("Hello world!"),
+            StringValue("This is a sentence. This is another sentence."),
+        ]
+
+        translations = self.translator.translate(
+            self.english_locale, self.french_locale, input_strings
+        )
+
+        expected_translations = {
             StringValue("Hello world!"): StringValue("Bonjour le monde!"),
             StringValue("This is a sentence. This is another sentence."): StringValue(
                 "Ceci est une phrase. Ceci est une autre phrase."
             ),
-        },
-    )
-    def test_translate_text(self, mock_translate):
-        self.assertIsInstance(self.translator, LibreTranslator)
+        }
 
-        translations = self.translator.translate(
-            self.english_locale,
-            self.french_locale,
-            {
-                StringValue("Hello world!"),
-                StringValue("This is a sentence. This is another sentence."),
-            },
-        )
+        # Assertions to check if the translation is as expected
+        self.assertEqual(translations, expected_translations)
 
-        self.assertEqual(
-            translations,
-            {
-                StringValue("Hello world!"): StringValue("Bonjour le monde!"),
-                StringValue(
-                    "This is a sentence. This is another sentence."
-                ): StringValue("Ceci est une phrase. Ceci est une autre phrase."),
-            },
-        )
-
-    @mock.patch(
-        "wagtail_localize.machine_translators.libretranslate.LibreTranslator.translate",
-        return_value={
-            StringValue('<a id="a1">Hello !</a>. <b>This is a test</b>.'): StringValue(
-                """<a id="a1">Bonjour !</a>. <b>C'est un test</b>."""
+        # Assert that requests.post was called with the correct arguments
+        mock_post.assert_called_once_with(
+            LIBRETRANSLATE_SETTINGS_ENDPOINT["OPTIONS"]["LIBRETRANSLATE_URL"]
+            + "/translate",
+            data=json.dumps(
+                {
+                    "q": [
+                        "Hello world!",
+                        "This is a sentence. This is another sentence.",
+                    ],
+                    "source": "en",
+                    "target": "fr",
+                    "api_key": LIBRETRANSLATE_SETTINGS_ENDPOINT["OPTIONS"]["API_KEY"],
+                }
             ),
-        },
-    )
-    def test_translate_html(self, mock_translate):
-        self.assertIsInstance(self.translator, LibreTranslator)
+            headers={"Content-Type": "application/json"},
+            timeout=10,
+        )
 
-        string, attrs = StringValue.from_source_html(
+    @mock.patch("requests.post")
+    def test_translate_html(self, mock_post):
+        # Mock the response of requests.post
+        mock_response = mock.Mock()
+        mock_response.json.return_value = {
+            "translatedText": ["""<a id="a1">Bonjour !</a>. <b>C'est un test</b>."""]
+        }
+        mock_response.raise_for_status = mock.Mock()
+        mock_post.return_value = mock_response
+
+        input_string, attrs = StringValue.from_source_html(
             '<a href="https://en.wikipedia.org/wiki/World">Hello !</a>. <b>This is a test</b>.'
         )
 
         translations = self.translator.translate(
-            self.english_locale, self.french_locale, [string]
+            self.english_locale, self.french_locale, [input_string]
         )
 
-        self.assertEqual(
-            translations[string].render_html(attrs),
-            """<a href="https://en.wikipedia.org/wiki/World">Bonjour !</a>. <b>C'est un test</b>.""",
+        expected_translation = {
+            input_string: StringValue(
+                """<a id="a1">Bonjour !</a>. <b>C'est un test</b>."""
+            )
+        }
+
+        # Assertions to check if the translation is as expected
+        self.assertEqual(translations, expected_translation)
+
+        # Additional assertion to check the rendered HTML
+        translated_string = translations[input_string]
+        rendered_html = translated_string.render_html(attrs)
+        expected_rendered_html = '<a href="https://en.wikipedia.org/wiki/World">Bonjour !</a>. <b>C\'est un test</b>.'
+
+        self.assertEqual(rendered_html, expected_rendered_html)
+
+        # Assert that requests.post was called with the correct arguments
+        mock_post.assert_called_once_with(
+            LIBRETRANSLATE_SETTINGS_ENDPOINT["OPTIONS"]["LIBRETRANSLATE_URL"]
+            + "/translate",
+            data=json.dumps(
+                {
+                    "q": [
+                        '<a id="a1">Hello !</a>. <b>This is a test</b>.'
+                    ],  # Use the string from StringValue
+                    "source": "en",
+                    "target": "fr",
+                    "api_key": LIBRETRANSLATE_SETTINGS_ENDPOINT["OPTIONS"]["API_KEY"],
+                }
+            ),
+            headers={"Content-Type": "application/json"},
+            timeout=10,
         )
 
     def test_can_translate(self):
