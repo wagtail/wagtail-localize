b955ae5841a4656f4fdf60f3bf51b474d77cdeed
zerolab <dan@zerolab.org>
2024-02-10 12:51:46 +0000

    Update version to 1.8

--- wagtail_localize/__init__.py
+++ wagtail_localize/__init__.py
@@ -2,6 +2,6 @@ from .version import get_version
 
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 8, 0, "beta", 1)
+VERSION = (1, 8, 0, "final", 1)
 
 __version__ = get_version(VERSION)
