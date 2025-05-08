30fbedab36bd7c0a210a0d8439924051055da141
zerolab <dan@zerolab.org>
2025-05-08 15:25:11 +0100

    Update CHANGELOG and bump version to 1.12.1

--- wagtail_localize/__init__.py
+++ wagtail_localize/__init__.py
@@ -2,6 +2,6 @@ from .version import get_version
 
 
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 12, 0, "final", 0)
+VERSION = (1, 12, 1, "final", 0)
 
 __version__ = get_version(VERSION)
